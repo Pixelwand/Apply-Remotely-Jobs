08f5d40002ef31ee8cd4fcfebc81846045845ac3
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-01-06 22:45:16 +0530

    Port and Database connected

--- backend/App.jsx
+++ backend/App.jsx
@@ -1,1 +1,22 @@
 const express = require('express');
+const mongoose  = require('mongoose');
+require('dotenv').config({path:'config.env'})
+
+const app = express();
+const port = process.env.PORT || 8000
+
+const DB = process.env.URI
+mongoose.connect(DB, {
+    useNewUrlParser: true
+}, ).then(()=>{
+    console.log("Connections Successful")
+}).catch((err)=>{
+    console.log(`Connection error: ${err}`)
+})
+
+
+app.listen(port, (req, res)=>{
+    console.log(`Port is running on ${port}`)
+} )
+
+module.exports = app;

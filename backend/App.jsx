89a5aba3c5dcb8663b90f0fc0f5da05b4cce0efe
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-01-06 23:38:28 +0530

    quick change

--- backend/App.jsx
+++ backend/App.jsx
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose  = require('mongoose');
 require('dotenv').config({path:'config.env'})
+const userRegistration = require('./routes/userRoutes.jsx')
 
 const app = express();
 const port = process.env.PORT || 8000
+
+//Connected to the MongoDB Database
 
 const DB = process.env.URI
 mongoose.connect(DB, {
@@ -14,6 +17,8 @@ mongoose.connect(DB, {
     console.log(`Connection error: ${err}`)
 })
 
+//Middleware
+app.use('/', userRegistration)
 
 app.listen(port, (req, res)=>{
     console.log(`Port is running on ${port}`)
---
+++ backend/routes/userRoutes.jsx
@@ -0,0 +1,11 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+var urlEncodedParser = bodyParser.urlencoded({extended:false})
+
+const app = express.Router();
+
+
+app.post(urlEncodedParser, (req, res)=>{
+    res.send('User account created successfully')
+})

ad3f473d276d6867553b3e64f198ca96fba47f1e
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-01-18 17:58:38 +0530

    Added CSS for Details component

--- backend/frontend/src/components/Work/Work.jsx
+++ backend/frontend/src/components/Work/Work.jsx
@@ -3,15 +3,15 @@ import { Workone, Worktwo, Workthree } from '../../Assets'
 export default function Work() {
   return (
     <>
-    <div class="bg-blue-900 text-white font-sans">
-        <div class="flex justify-center gap-60 flex-row max-w-7xl mx-auto px-28 py-12">
-            <div class="basis-1/2 font-bold text-5xl">Ways to work with us</div>
-              <div>
-            <button class="basis-1/2 bg-blue-600 px-8 py-4 hover:bg-blue-500">Get Started</button>
+    <div class="bg-blue-900 text-white font-sans w-full">
+        <div class="flex flex-row px-6 gap-6 py-12">
+            <div class=" basis-1/2 font-bold text-2xl ">Ways to work with us</div>
+              <div className='basis-1/2'>
+            <button class=" bg-blue-600 px-8 py-4 hover:bg-blue-500">Get Started</button>
             </div>
         </div>
         <div class="pb-10">
-        <div class="flex flex-row justify-center gap-20 mx-auto max-w-7xl px-4 sm:px-6">
+        <div class="flex flex-col justify-center gap-20 mx-auto max-w-7xl px-4 sm:px-6">
       <div class="basis-1/4">
         <div>
           <div class="h-14 w-14 absolute float-right bg-blue-600"></div>

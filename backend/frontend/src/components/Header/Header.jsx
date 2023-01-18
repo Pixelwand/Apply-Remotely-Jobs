3b445e093c2e4a68a5f0ba0f4ae3ca5d7747f359
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-01-18 13:19:01 +0530

    quick css change

--- backend/frontend/src/components/Header/Header.jsx
+++ backend/frontend/src/components/Header/Header.jsx
@@ -36,14 +36,13 @@ export default function Header() {
   return (
     <>
     
-<nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+<nav class="bg-white  block px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
   <a href="http://localhost:3000" class="flex items-center">
-      <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">ARJ</span>
+      <span class="self-center text-xl font-bold whitespace-nowrap ml-4 dark:text-white">ARJ</span>
   </a>
   <div class="flex md:order-2">
-      <button type="button" onClick={openModal} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+      <button type="button" onClick={openModal} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Get started</button>
 
      
 
@@ -69,8 +68,7 @@ export default function Header() {
     </ul>
   </div>
   </div>
-</nav>
-<Modal class="z-20"
+  <Modal class="z-2"
         isOpen={isModalOpen}
         onRequestClose={closeModal}
         style={customStyles}
@@ -96,6 +94,8 @@ export default function Header() {
           </button>
         </form>
         </Modal>
+
+</nav>
 
     </>
   )

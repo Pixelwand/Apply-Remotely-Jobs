2dcededcd54ac61d631e2474de5a4a84b428b766
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-01-18 18:37:43 +0530

    Added responsive css for mobile screen in Resource component

--- backend/frontend/src/components/Resources/Resources.jsx
+++ backend/frontend/src/components/Resources/Resources.jsx
@@ -4,20 +4,20 @@ export default function Resources() {
   return (
     <>
   
-    <div class="flex justify-center flex-row mx-auto max-w-7xl px-4c mt-10 sm:px-6">
+    <div class="flex justify-center flex-row mx-auto w-full px-4c mt-32 ">
     <div class="basis-1/2 text-xl">
-        <div class="text-5xl font-sans font-bold">Resources</div>
+        <div class="text-3xl font-sans font-bold">Resources</div>
     </div>
-    <div class="left-40 relative">
-    <button class="basis-1/2 bg-blue-700 text-white px-8  py-4 hover:bg-blue-800">Get Started</button>
+    <div class="">
+    <button class="basis-1/2 rounded-5 bg-blue-700 text-white px-8  py-4 hover:bg-blue-800">Get Started</button>
     </div>
     </div>
     <br/>
     <br/>
-    <div class="flex flex-row justify-center gap-20 mx-auto max-w-7xl px-4 sm:px-6">
+    <div class="flex flex-col justify-center gap-20 mx-auto max-w-7xl px-4 sm:px-6">
       <div class="basis-1/4">
         <div>
-          <img src={Resone} alt="Image-1" class="w-76 h-80 top-6 left-6" />
+          <img src={Resone}  class="w-76 h-80 top-6 left-6" alt="Resource one" />
         </div>
         <div class="w-76 h-43 pr-4.5 pl-0 pt-0 pb-0 mt-8">
         <h2 class="font-bold text-2xl mb-5">A high converting, high-performing template</h2>
@@ -31,7 +31,7 @@ export default function Resources() {
       </div>
       <div class="basis-1/4">
       <div>
-          <img src={Restwo} alt="Image-2"  class="w-76 h-80 top-6 left-6" />
+          <img src={Restwo} alt="Resource two"  class="w-76 h-80 top-6 left-6" />
         </div>
         <div class="w-76 h-43 pr-4.5 pl-0 pt-0 pb-0 mt-8">
         <h2  class="font-bold text-2xl mb-5">With a clean, minimal and professional look</h2>
@@ -45,7 +45,7 @@ export default function Resources() {
       </div>
       <div class="basis-1/4">
       <div>
-          <img src={Resthree} alt="Image-3"  class="w-76 h-80 top-6 left-6" />
+          <img src={Resthree} alt="Resource three"  class="w-76 h-80 top-6 left-6" />
         </div>
         <div  class="w-76 h-43 pr-4.5 pl-0 pt-0 pb-0 mt-8">
         <h2  class="font-bold text-2xl mb-5">Opus made our journey possible</h2>

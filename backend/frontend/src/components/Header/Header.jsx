341935044c74d59c5e47b7312d5febf7c0a7c75d
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-01-19 16:21:53 +0530

    quick fix

--- backend/frontend/src/components/Header/Header.jsx
+++ backend/frontend/src/components/Header/Header.jsx
@@ -63,7 +63,7 @@ export default function Header() {
   </a>
   <div class="flex md:order-2">
       <button type="button" onClick={openModal} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 text-center ml-28 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Get started</button>
-      <button data-collapse-toggle="navbar-sticky"  type="button" class="inline-flex ml-4 items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false"  id='myDropdown'>
+      <button data-collapse-toggle="navbar-sticky" onClick={onExpand}  type="button" class="inline-flex ml-4 items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false"  id='myDropdown'>
         <span class="sr-only">Open main menu</span>
         <svg class="w-6 h-6 " aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
@@ -124,11 +124,11 @@ const navbarsticky = document.getElementById("navbar-sticky");
 const myDropdown = document.getElementById('myDropdown');
 
 // optional options with default values and callback functions
-const options = {
-  onExpand: (navbarsticky, myDropdown) => {
+
+  function onExpand(navbarsticky, myDropdown){
     return true
   }
-};
+
 
   
 

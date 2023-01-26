e52fbe2e2f00a883f8fa49638fdca3093a57a490
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-01-26 12:04:47 +0530

    quick change

--- backend/controllers/postJob.js
+++ backend/controllers/postJob.js
@@ -1,11 +1,11 @@
 const Jobpost = require('../models/postJobModel');
 
 exports.createJobpost = async(req, res)=>{
-try{ await Jobpost.create(req.body);
+try{const Job =  await Jobpost.create(req.body);
     console.log(req.body);
     
     res.status(201).send({
-        message:"New Job Post Added Succesfully"
+        message:"New Job Post Added Succesfully."
     })
 } catch{
     res.status(501).send({

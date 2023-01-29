c7ae6c78d4f41129f0338b3d5c71af8b1ddfa3c9
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-01-29 09:58:36 +0530

    css for sm screen in main compnent

--- backend/frontend/src/components/Main/Main.jsx
+++ backend/frontend/src/components/Main/Main.jsx
@@ -5,26 +5,26 @@ import { ProfileMain } from '../../Assets';
 export default function Main() {
   return (
     <>
-    <div class="mt-3 mb-72 flex flex-wrap sm:bg-black">
-      <div class="flex flex-row flex-wrap">
-        <div class="bg-indigo-900 text-white pt-5 pb-16   ">
-          <div class="font-bold text-4xl w-full  text-center mx-auto my-auto">Find High Paying Remote Jobs</div>
+    <div className="mt-3 mb-72 flex flex-wrap">
+      <div className="flex flex-row flex-wrap">
+        <div className="bg-indigo-900 text-white pt-10 pb-20  ">
+          <div className="font-bold text-4xl w-full  text-center mx-auto my-auto">Find High Paying Remote Jobs</div>
         
-          <p class="text-center mt-10 mx-4 my-auto text-lg font-normal leading-9">Find 100+ Remote Jobs for more than 20+ category from Entry level to Senior level and work from anywhere.</p>
-          <div class="text-center mt-10">
-            <span class="mr-5">
-             <a href='http://localhost:3000/postjobs' className='bg-green-600 p-4 rounded-xl font-bold font-sans'>Post Remote Jobs </a>
+          <p className="text-center mt-10 mx-4 sm:mx-auto sm: sm:w-4/6 sm:justify-center sm:text-2xl  my-auto text-lg font-normal leading-9">Find 100+ Remote Jobs for more than 20+ category from Entry level to Senior level and work from anywhere.</p>
+          <div className="text-center mt-10">
+            <span className="mr-5 sm:mr-10">
+             <a href='http://localhost:3000/postjobs' className='bg-green-600 p-4 rounded-xl font-bold font-sans sm:text-2xl'>Post Remote Jobs </a>
             </span>
-            <a href='http://localhost:3000/jobs' className='bg-green-600 p-4 rounded-xl font-bold font-sans'>Find Remote Jobs</a>
+            <a href='http://localhost:3000/jobs' className='bg-green-600 p-4 rounded-xl font-bold font-sans sm:text-2xl'>Find Remote Jobs</a>
           </div>
           </div>
-          <div class="w-full mb-28">
+          <div className="w-full mb-28">
           <img src={ProfileMain} class="object-fill" alt="ProfileMain" />
           
-        <div class="bg-white text-black  absolute mt-4  shadow-2xl rounded-3xl ">
-          <div class="font-normal p-10 text-lg text-center">I felt like I couldn’t find qualified candidates until I moved to Kriyax. Now I am encouraged to find more qualified candidates with Apply Remotely Jobs.</div>
-          <div class="font-bold text-base text-center">Deependra Kumar</div>
-          <div class="font-medium text-center py-3">CEO @KRIYAX</div>
+        <div className="bg-white text-black  absolute mt-4  shadow-2xl rounded-3xl ">
+          <div className="font-normal p-10 text-lg text-center">I felt like I couldn’t find qualified candidates until I moved to Kriyax. Now I am encouraged to find more qualified candidates with Apply Remotely Jobs.</div>
+          <div className="font-bold text-base text-center">Deependra Kumar</div>
+          <div className="font-medium text-center py-3">CEO @KRIYAX</div>
         </div>
         </div>
       </div>

7608fbcf748382a7b63ec84d02bfeb1f5b22e6de
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-02-03 14:27:44 +0530

    updated homepage address

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -3,7 +3,6 @@ const Jobs = require('../models/postJobModel');
 exports.getJobs = async(req, res)=>{
    try{await Jobs.find().then(
    result=>res.status(200).send({
-    message:"Jobs data found successfully",
     listJobs:result
    }))
    
--- backend/frontend/src/components/Jobs/Jobs.jsx
+++ backend/frontend/src/components/Jobs/Jobs.jsx
@@ -8,19 +8,29 @@ export const Jobs = () => {
    fetch(`http://localhost:8080/user/jobs`, {method:'GET'})
    .then(response=>response.json())
    .then(data=>{
-    setJobs(data.listJobs)
+    // console.log("data:", data)
+    setJobs({data:data.listJobs})
    })
   }, [])
   
-  console.log("jobs:", jobs)
-  const {companyName, companyEmail, applyUrl, howtoapply, jobDescription, jobtype,  minSalary, maxSalary, position, primaryField, secondEmail, twitterUsername}=jobs;
-  console.log("Company Name", companyName);
+  console.log("listJobs:", jobs)
+  // const {companyName, companyEmail, applyUrl, howtoapply, jobDescription, jobtype,  minSalary, maxSalary, position, primaryField, secondEmail, twitterUsername}=jobs;
+  // console.log("Company Name", companyName);
   return (
     <>
     <div>List of all Remote Jobs</div>
-    <div className="text-blue-600">{companyName}</div>
-    <div>{companyEmail}</div>
-    <div>{applyUrl}</div>
+    {/* <div>{companyName}</div> */}
+    {/* <div className="text-blue-600">{companyName}</div> */}
+    {/* <div>{companyEmail}</div> */}
+    {/* <div>{applyUrl}</div> */}
+    
+        <div>
+        <ul>
+          {jobs.map(job => (
+            <li key={job.id}>{job.companyName}</li>
+          ))}
+        </ul>
+    </div>
     </>
   )
 }

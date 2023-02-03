bfc76632d13e0ef19fc48507de045b4c13263424
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-02-03 15:47:38 +0530

    started displaying data on the client side by fetching from backend

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -2,9 +2,7 @@ const Jobs = require('../models/postJobModel');
 
 exports.getJobs = async(req, res)=>{
    try{await Jobs.find().then(
-   result=>res.status(200).send({
-    listJobs:result
-   }))
+   result=>res.status(200).send(result))
    
 } catch{
    res.status(501).send("Internal Server Error")
--- backend/frontend/src/components/Jobs/Jobs.jsx
+++ backend/frontend/src/components/Jobs/Jobs.jsx
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from "react"
 
 export const Jobs = () => {
-  const [jobs, setJobs] = useState({})
+  const [jobData, setJobData] = useState([])
 
 
   useEffect(() => {
-   fetch(`http://localhost:8080/user/jobs`, {method:'GET'})
-   .then(response=>response.json())
-   .then(data=>{
-    // console.log("data:", data)
-    setJobs({data:data.listJobs})
-   })
+ fetch(`http://localhost:8080/user/jobs`, {method:'GET'})
+   .then((response)=>response.json())
+   .then((data)=>setJobData(data))
   }, [])
   
-  console.log("listJobs:", jobs)
+  console.log("listJob:", jobData)
+  // const {companyName, companyEmail} = jobData
   // const {companyName, companyEmail, applyUrl, howtoapply, jobDescription, jobtype,  minSalary, maxSalary, position, primaryField, secondEmail, twitterUsername}=jobs;
   // console.log("Company Name", companyName);
   return (
     <>
     <div>List of all Remote Jobs</div>
+    {/* <div>{`${companyName}`}</div> */}
+    <div>
+      {jobData.map(job=>
+        <li key={job.id}>{job.companyName}</li>
+        )}
+    </div>
+    {/* <div>{companyEmail}</div> */}
     {/* <div>{companyName}</div> */}
     {/* <div className="text-blue-600">{companyName}</div> */}
     {/* <div>{companyEmail}</div> */}
     {/* <div>{applyUrl}</div> */}
     
         <div>
-        <ul>
-          {jobs.map(job => (
+        {/* <ul>
+          {jobData((job) => (
             <li key={job.id}>{job.companyName}</li>
           ))}
-        </ul>
+        </ul> */}
     </div>
     </>
   )

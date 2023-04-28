a37f3e0e2dca347113d93ec41c8ee1617c94e774
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-04-28 18:43:13 +0530

    bug: fixed postjob controller on server side to post job

--- ARJ/models/postJobModel.js
+++ ARJ/models/postJobModel.js
@@ -1,114 +1,58 @@
-// const mongoose = require('mongoose');
-// const jobSchema = new mongoose.Schema({
-//     companyName:{
-//         type:String,
-//         require:true
-//     },
-//     companyEmail:{
-//         type:String,
-//         require:true
-//     },
-//     position:{
-//         type:String,
-//         require:true
-//     },
-//     jobtype:{
-//         type:Object
-//     },
-//     primaryField:{
-//         type:Object
-//     },
-//     techStack:{
-//         type:String
-//     },
-//     locations:{
-//         type:Object,
-//     },
-//     companyLogo:{
-//     type:String
-//     },
-//     minSalary:{
-//         type:Object
-//     },
-//     maxSalary:{
-//         type:Object,
-//         require:true
-//     },
-//     jobDescription:{
-//         type:String,
-//         require:true
-//     },
-//     howtoapply:{
-//         type:String
-//     },
-//     applyUrl:{
-//         type:String,
-//         require:true
-//     },
-//     twitterUsername:{
-//         type:String
-//     },
-//     secondEmail:{
-//         type:String
-//     }
-
-// })
-
-// const Jobpost = mongoose.model("Jobpost", jobSchema)
-// module.exports = Jobpost;
-
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const jobSchema = new mongoose.Schema({
-  abstractTitle: {
-    type: String,
-    require: true,
-  },
-  authorName: {
-    type: String,
-    require: true,
-  },
-  authorEmail: {
-    type: String,
-    require: true,
-  },
+    companyName:{
+        type:String,
+        require:true
+    },
+    companyEmail:{
+        type:String,
+        require:true
+    },
+    position:{
+        type:String,
+        require:true
+    },
+    jobtype:{
+        type:Object
+    },
+    primaryField:{
+        type:Object
+    },
+    techStack:{
+        type:String
+    },
+    locations:{
+        type:Object,
+    },
+    companyLogo:{
+    type:String
+    },
+    minSalary:{
+        type:Object
+    },
+    maxSalary:{
+        type:Object,
+        require:true
+    },
+    jobDescription:{
+        type:String,
+        require:true
+    },
+    howtoapply:{
+        type:String
+    },
+    applyUrl:{
+        type:String,
+        require:true
+    },
+    twitterUsername:{
+        type:String
+    },
+    secondEmail:{
+        type:String
+    }
 
-  authorNumber: {
-    type: Number,
-    require: true,
-  },
-  authorOrganization: {
-    type: String,
-    require: true,
-  },
-  authorCountry: {
-    type: String,
-    require: true,
-  },
-  otherAuthorName: {
-    type: String,
-    require: true,
-  },
-  otherAuthorEmail: {
-    type: String,
-    require: true,
-  },
-  otherAuthorNumber: {
-    type: Number,
-    require: true,
-  },
-  otherAuthorOrganization: {
-    type: String,
-    require: true,
-  },
-  otherAuthorCountry: {
-    type: String,
-    require: true,
-  },
-  abstractFile: {
-    type: String,
-    require: true,
-  },
-});
+})
 
-const Jobpost = mongoose.model("Jobpost", jobSchema);
+const Jobpost = mongoose.model("Jobpost", jobSchema)
 module.exports = Jobpost;

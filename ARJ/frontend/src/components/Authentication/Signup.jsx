07aac2484eb2f6a6d2daf9a365d616a25841565a
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-06-20 00:27:25 +0530

    fixed eslint error

--- ARJ/frontend/src/components/Authentication/Signup.jsx
+++ ARJ/frontend/src/components/Authentication/Signup.jsx
@@ -75,6 +75,7 @@ export const Signup = () => {
                 {...register("email", {
                   required: true,
                   pattern:
+                    // eslint-disable-next-line no-useless-escape
                     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                 })}
               />
--- ARJ/frontend/src/components/Jobs/PostJobs.jsx
+++ ARJ/frontend/src/components/Jobs/PostJobs.jsx
@@ -176,6 +176,7 @@ export const PostJobs = () => {
                   { required: true },
                   {
                     pattern:
+                      // eslint-disable-next-line no-useless-escape
                       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                   }
                 )}
